1a848cc383976a4c19664a0a0da1e13efd4957f8
ahsel21 <180645@edu.psuti.ru>
2019-09-19 16:11:51 +0300

    Added entry point

--- PO73.Kiptenko.wdad/Application.java
+++ PO73.Kiptenko.wdad/Application.java
@@ -1,5 +1,5 @@
 public class Application {
     public static void main(String[] args) {
-        System.out.println("I’m Алексей Киптенко, and I’m not a monkey");
+        System.out.println("I’m Алексей Киптенко, I’m not a monkey");
     }
 }
